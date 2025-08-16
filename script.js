18060ca98ae6c8bcc335203734d25b01260b6e0e
Thulile <THULILEMASUKU@YAHOO.COM>
2025-08-16 17:26:16 +0200

    Move dom-manipulation files to root for course check

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rename function
+function displayRandomQuote() { ... }
+
+// Update button
+newQuoteBtn.id = "newQuoteBtn";
+newQuoteBtn.addEventListener("click", displayRandomQuote);
+
+// Also call initial display
+displayRandomQuote();
